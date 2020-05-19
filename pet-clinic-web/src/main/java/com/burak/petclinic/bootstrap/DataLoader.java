71f1d9d2b3ec1e2898d6972fb7df7f8f70b83626
burakmutluay <mburakmutluay@gmail.com>
2020-05-19 13:48:46 +0300

    Added Owners List and updated controller. Closes #16

--- pet-clinic-web/src/main/java/com/burak/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/burak/petclinic/bootstrap/DataLoader.java
@@ -13,7 +13,7 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-
+    
     public DataLoader(OwnerService ownerService, VetService vetService){
         this.ownerService = ownerService;
         this.vetService = vetService;
@@ -21,6 +21,7 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
         Owner owner1 = new Owner();
         owner1.setId(1L);
         owner1.setFirstName("Michael");
@@ -29,13 +30,15 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Fiona");
-        owner1.setLastName("Glenanne");
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
 
         ownerService.save(owner2);
 
         System.out.println("Loading Owners....");
+
+        System.out.println(ownerService.findAll());
 
         Vet vet1 = new Vet();
         vet1.setId(1L);
